16e1a64f49e139b2d375e27ceb34694f09e33d1f
AimenSattar <73948355+AimenSattar@users.noreply.github.com>
2025-03-06 23:31:20 +0000

    Update userReadSwatOutput.R

--- R/userReadSwatOutput.R
+++ R/userReadSwatOutput.R
@@ -13,15 +13,39 @@
 #'
 #' @export
 
+library(dplyr)
 
-   userReadSwatOutput <- function(workingDirectory,
-                               coreNumber,
-                               fileName,
-                               output){
-    
-    
-    filePath <- paste0(workingDirectory, paste0("/TxtInOut_", coreNumber,"/"), fileName)
+userReadSwatOutput <- function(workingDirectory, coreNumber, fileName, output) {
+  if (fileName == "basin_crop_yld_yr.txt") {
+    filePath <- paste0(workingDirectory, paste0("/TxtInOut_", coreNumber, "/"), fileName)
     output <- read.table(filePath, header = TRUE, sep = "", skip = 1)
     return(output)
+  } else {
+    # Read irrigation file
+    filePath <- paste0(workingDirectory, paste0("/TxtInOut_", coreNumber, "/"), fileName)
+    Lines <- readLines(filePath)
+    Lines <- Lines[-1]
+    writeLines(Lines, filePath)
+    irrigation <- read.table(filePath, header = TRUE, sep = "", skip = 1)
+    irrigation <- irrigation %>% select(yr, unit, irr)
+    
+    # Read HRU file
+    hru_file <- paste0(workingDirectory, paste0("/TxtInOut_", coreNumber, "/"), "hru-data.hru")
+    hru <- read.table(hru_file, header = TRUE, sep = "", skip = 2)
+    
+    # Process HRU data
+    hru_filtered <- hru %>%
+      select(id, lu_mgt) %>%
+      filter(!lu_mgt %in% c("wetw_lum", "wetn_lum", "bsvg_lum", "urhd_lum", "swrn_lum"))
+    
+    # Match irrigation data with filtered HRU data
+    output <- irrigation %>%
+      inner_join(hru_filtered, by = c("unit" = "id")) %>%
+      mutate(lu_mgt = gsub("_lum$", "", lu_mgt)) %>%
+      select(unit, yr, irr, lu_mgt)
+    
+    return(output)
+  }
 }
+
   
